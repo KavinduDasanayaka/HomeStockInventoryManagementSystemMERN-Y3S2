c6e3113b113e2bfca5b000edf86113e10fa4b5bc
Madutha123 <165867586+Madutha123@users.noreply.github.com>
2025-03-22 20:24:40 +0530

    set few agestments and set font.

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {
+
+        fontFamily: {
+          sans: ["Poppins", "sans-serif"],
+          cursive: ["Pacifico", "Sriracha", "cursive"],
+          cursive2: ["Sriracha", "cursive"],
+  },
       keyframes: {
         beatingHeart: {
           '0%': { transform: 'scale(1)' },
