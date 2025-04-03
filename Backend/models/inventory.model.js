9b58d5c6fca6c6d6799004841f42ac1a443e2228
Dilakshi Thilini <IT22310446@my.sliit.lk>
2025-04-03 14:48:04 +0530

    fixed

--- Backend/models/inventory.model.js
+++ Backend/models/inventory.model.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const inventorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   quantity: { type: Number, required: true },
+  expireDate: { type: Date, required: true }, // Added expireDate field
 });
 
 export const Inventory = mongoose.model('Inventory', inventorySchema);
